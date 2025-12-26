d7358059ef3284062e3f91d4638a393c884c1104
Kay <129507383+2638152345@users.noreply.github.com>
2025-12-26 14:30:07 +0800

    Add task1.1
    Implement a function to find the single number in an array.

--- Task1/task1.go
+++ Task1/task1.go
@@ -1,1 +1,29 @@
+package main
+
+import "fmt"
+
+func singleNumber(nums []int) int {
+    for i := 0; i < len(nums); i++ {
+        count := 0
+        for j := 0; j < len(nums); j++ {
+            if nums[i] == nums[j] {
+                count++
+            }
+        }
+        if count == 1 {
+            return nums[i]
+        }
+    }
+    return 0
+}
+
+func main() {
+    var n int
+    fmt.Scan(&n)
+    nums := make([]int, n)
+    for i := 0; i < n; i++ {
+        fmt.Scan(&nums[i])
+    }
+    fmt.Println(singleNumber(nums))
+}
 
