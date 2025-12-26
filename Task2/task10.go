15648230673acb273e08dfd4eeadb12b7432b9a8
Kay <129507383+2638152345@users.noreply.github.com>
2025-12-26 14:41:41 +0800

    Implement task10.go

---
+++ Task2/task10.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "fmt"
+    "sync"
+    "sync/atomic"
+)
+
+func main() {
+    var counter int64
+    var wg sync.WaitGroup
+
+    numGoroutines := 10
+    numIncrements := 1000
+
+    wg.Add(numGoroutines)
+
+    for i := 0; i < numGoroutines; i++ {
+        go func() {
+            defer wg.Done()
+            for j := 0; j < numIncrements; j++ {
+                atomic.AddInt64(&counter, 1)
+            }
+        }()
+    }
+
+    wg.Wait()
+    fmt.Println("最终计数器值:", counter)
+}
