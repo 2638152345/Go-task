62ec86500233a4b7472a50c7ec3a9d4ba1d2bb8a
Kay <129507383+2638152345@users.noreply.github.com>
2025-12-26 14:38:21 +0800

    Add task3.go

---
+++ Task2/task3.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+    "fmt"
+    "sync"
+)
+
+func printOdd(wg *sync.WaitGroup) {
+    defer wg.Done()
+    for i := 1; i <= 10; i++ {
+        if i%2 == 1 {
+            fmt.Println(i)
+        }
+    }
+}
+
+func printEven(wg *sync.WaitGroup) {
+    defer wg.Done()
+    for i := 2; i <= 10; i++ {
+        if i%2 == 0 {
+            fmt.Println(i)
+        }
+    }
+}
+
+func main() {
+    var wg sync.WaitGroup
+    wg.Add(2)
+
+    go printOdd(&wg)
+    go printEven(&wg)
+
+    wg.Wait()
+}
