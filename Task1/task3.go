3e3402e35c12feb65cff52179732bb0d09aab130
Kay <129507383+2638152345@users.noreply.github.com>
2025-12-26 14:31:31 +0800

    Implement  task3.go

---
+++ Task1/task3.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func isValid(s string) bool {
+    stack := []rune{}
+    for _, c := range s {
+        if c == '(' || c == '[' || c == '{' {
+            stack = append(stack, c)
+        } else {
+            if len(stack) == 0 {
+                return false
+            }
+            top := stack[len(stack)-1]
+            stack = stack[:len(stack)-1]
+            if (c == ')' && top != '(') || 
+               (c == ']' && top != '[') || 
+               (c == '}' && top != '{') {
+                return false
+            }
+        }
+    }
+    return len(stack) == 0
+}
+
+func main() {
+    var s string
+    fmt.Scan(&s)
+    fmt.Println(isValid(s))
+}
